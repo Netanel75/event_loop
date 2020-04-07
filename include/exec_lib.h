322e64dcc03f210ef3b4d1662f2455ce51a49bfc
Netanel <netanel75@gmail.com>
2020-04-07 16:18:34 +0300

    add callbacks to asynchronic calls

--- include/exec_lib.h
+++ include/exec_lib.h
@@ -5,12 +5,11 @@
 
 #include "mbuf.h"
 
-typedef void (*async_cb_t)(struct mbuf *buffer);
 typedef int (*sync_cb_t)(struct mbuf *buffer);
 
 int exec_proccess(char **cmd, bool is_async, sync_cb_t sync_cb);
 
-void init_event_loop(async_cb_t async);
+void init_event_loop(void);
 
 void start_event_loop(unsigned int number_of_dockers);
 
